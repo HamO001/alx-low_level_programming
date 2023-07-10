6e9f6fa0c6b3d7b84fbbd6e72897b821efaaba00
HamO001 <ndarugastiv9@gmail.com>
2023-07-10 14:33:13 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,81 +1,103 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-
-/**
- * error_exit - Prints an error message and exits with the specified code.
- * @code: The exit code.
- * @msg: The error message to print.
- */
-void error_exit(int code, const char *msg)
-{
-	dprintf(STDERR_FILENO, "%s\n", msg);
-	exit(code);
-}
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * copy_file - Copies the content of one file to another file.
- * @src_file: The source file to read from.
- * @dest_file: The destination file to write to.
- */
-void copy_file(const char *src_file, const char *dest_file)
-{
-	int fd_src, fd_dest, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
-	fd_src = open(src_file, O_RDONLY);
-	if (fd_src == -1)
-		error_exit(98, "Error: Can't read from file");
-
-	fd_dest = open(dest_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (fd_dest == -1)
-	{
-		close(fd_src);
-		error_exit(99, "Error: Can't write to file");
-	}
-
-	while ((bytes_read = read(fd_src, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_dest, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			close(fd_src);
-			close(fd_dest);
-			error_exit(99, "Error: Can't write to file");
-		}
-	}
-
-	if (bytes_read == -1)
-	{
-		close(fd_src);
-		close(fd_dest);
-		error_exit(98, "Error: Can't read from file");
-	}
-
-	if (close(fd_src) == -1)
-		error_exit(100, "Error: Can't close source file descriptor");
-
-	if (close(fd_dest) == -1)
-		error_exit(100, "Error: Can't close destination file descriptor");
-}
-
-/**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
  *
- * Return: 0 on success, or an error code.
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to");
+	int from, to, r, w;
+	char *buffer;
 
-	copy_file(argv[1], argv[2]);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
